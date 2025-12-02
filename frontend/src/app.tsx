614866eda060fa26d7f7c80eee25f0d972f1aebc
leonwigley <leoncharleswigley@gmail.com>
2025-12-02 09:39:38 +0000

    Updated UI and backend

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,31 +1,37 @@
 import { useState, useEffect } from 'preact/hooks'
 import waterstonsLogo from './assets/logo.svg'
 import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+import dotnetLogo from './assets/dotnet.png'
+import './assets/light.css'
 import './app.css'
 
-function CatList() {
-  const [cats, setCats] = useState([]);
+interface Cat {
+  name: string;
+}
+
+interface CatListProps {
+  count: number;
+}
+
+function CatList({ count }: CatListProps) {
+  const [cats, setCats] = useState<Cat[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:5075/cats')
       .then((res) => res.json())
-      .then((data) => {
+      .then((data: Cat[]) => {
         setCats(data);
         setLoading(false);
       })
-      .catch((err) => {
-        console.error('Error fetching cats:', err);
-        setLoading(false);
-      });
+      .catch(() => setLoading(false));
   }, []);
 
   if (loading) return <div>Loading...</div>;
 
   return (
     <ul>
-      {cats.map((cat) => (
+      {cats.slice(0, count).map((cat) => (
         <li key={cat.name}>{cat.name}</li>
       ))}
     </ul>
@@ -33,23 +39,39 @@ function CatList() {
 }
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(3);
 
   return (
     <>
-      <div>
+      <nav>
         <a href="https://vite.dev" target="_blank">
           <img src={waterstonsLogo} class="logo" alt="Waterstons logo" />
         </a>
+        <p>Hi Mike!</p>
+      </nav>
+
+      <div class="builtWith">
+        <h2>This app is built with:</h2>
         <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+          <img src={preactLogo} alt="Preact logo" />
+        </a>
+        <a href="https://dotnet.microsoft.com/en-us/apps/aspnet" target="_blank">
+          <img src={dotnetLogo} alt="ASP.NET Core logo" />
         </a>
       </div>
-      <h1>Welcome to the Cats app!</h1>
-      <p>The API we fetch from: <a href="http://localhost:5075/cats">http://localhost:5075/cats</a></p>
-      <div class="card">
-        <CatList />
-      </div> 
+
+      <main>
+        <h1>Welcome to the Cats app!</h1>
+        <p>The API we fetch from: <a href="http://localhost:5075/cats">http://localhost:5075/cats</a></p>
+
+        <div class="card">
+          <span >You have: {count} cats</span>
+          <p>More cats?</p>
+          <button onClick={() => setCount((c) => Math.max(c - 1, 0))}>-</button>
+          <button onClick={() => setCount((c) => c + 1)}>+</button>
+          <CatList count={count} />
+        </div>
+      </main>
     </>
   )
 }
